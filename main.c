34f7ed22b8f64298482c63f677a0e01d18c2f74a
Filip Schwarz <filipschwarz007@gmail.com>
2021-09-14 23:22:32 +0200

    generated vscode addon

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * This is just a test C file for this project. 
+ * It's contents have to be compileable.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World!");
+    return 0;
+}
